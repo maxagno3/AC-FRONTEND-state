37c6b57289479ac2ea0d8bdcd590c0bdb13fd7f8
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2020-06-12 19:53:02 +0530

    counter done. css remain

--- src/accordian/Main.js
+++ src/accordian/Main.js
@@ -25,6 +25,7 @@ class App extends React.Component {
         >
           JavaScript
         </button>
+        
         <button
           onClick={() =>
             this.setState({showMessage: 'html'})
--- src/counter/Main.js
+++ src/counter/Main.js
@@ -1,5 +1,52 @@
 import React from 'react'
+import Button from './components/Button'
+import Heading from './components/Heading'
 
-export default function App() {
-  return <h1>Hello Counter</h1>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+      name: 'Counter',
+    }
+  }
+
+  handleInc = () => {
+    //   Changing the state.
+    this.setState(state => ({
+      count: state.count + 1,
+    }))
+  }
+
+  handleDec = () => {
+    this.setState(state => ({
+      count: state.count - 1,
+    }))
+  }
+
+  handleReset = () => {
+    this.setState(state => ({
+      count: 0,
+    }))
+  }
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.name}</h1>
+        {/* <h3>{this.state.count}</h3> */}
+        <Heading>{this.state.count}</Heading>
+        <Button handleClick={this.handleInc}>
+          Increment
+        </Button>
+        <Button handleClick={this.handleDec}>
+          Decrement
+        </Button>
+        <Button handleClick={this.handleReset}>
+          Reset
+        </Button>
+      </>
+    )
+  }
 }
+export default App
